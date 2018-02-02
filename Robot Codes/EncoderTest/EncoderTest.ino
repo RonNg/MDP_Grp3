91a4b2f4daa98852f74c22016a2506092bc968d5
Ron Ng <ronngjianying@gmail.com>
2018-02-02 14:49:48 +0800

    Robot 90 deg right rotation done in EncoderTest.ino
    Signed-off-by: Ron Ng <ronngjianying@gmail.com>

--- Robot Codes/EncoderTest/EncoderTest.ino
+++ Robot Codes/EncoderTest/EncoderTest.ino
@@ -20,15 +20,15 @@ int m1TargetCounter = 0; //For counting to target ticks bnased on distance
 int m2TargetCounter = 0;
 
 
-#define TICK_REFRESH_INTERVAL 100 //ms
+#define TICK_REFRESH_INTERVAL 1000 //ms
 #define TICKS_PER_CM 119 
 #define TICKS_PER_REV 2248.86
 
 unsigned long lastTime = 0;
 
 
-double m1Speed = 300;
-double m2Speed = 300;
+double m1Speed = 0;
+double m2Speed = 0;
 
 double errSum = 0;
 double lastErr = 0;
@@ -41,6 +41,15 @@ void calcTicks(int refreshInterval)
 {
     if ((millis() - lastTime) >=  refreshInterval)
     {
+      /*
+      Serial.print("Left Tick: ");
+      Serial.print(m1Ticks);
+      
+      Serial.print(", Right Ticks: ");
+      Serial.print(m2Ticks);
+      Serial.println();
+    */
+      
       m1RPM = ((m1Ticks*(60000/refreshInterval))/TICKS_PER_REV); //Extrapolate RPM
       m2RPM = ((m2Ticks*(60000/refreshInterval))/TICKS_PER_REV); //100ms * 600ms = 1 minute (interpolate RPM)
       m1Ticks = 0;
@@ -49,7 +58,6 @@ void calcTicks(int refreshInterval)
       lastTime = millis();
     }   
 }
-
 
 void ResetPID()
 {
@@ -84,16 +92,19 @@ double computePID(double consKp, double consKi, double consKd, char direction, d
 void TestStraight()
 {
     calcTicks (TICK_REFRESH_INTERVAL);
-  
+
+    /*
     Serial.print("Left RPM: ");
     Serial.print(m1RPM);
     
     Serial.print(", Right RPM: ");
     Serial.print(m2RPM);
     Serial.println();
+    */
 
-    m1Speed += computePID(0.00005, 0, 0.08, 'f', 80, 1);
-    m2Speed += computePID(0.00008, 0, 0.08, 'f', 80, 2);
+    md.setSpeeds(200, 200);
+    //m1Speed += computePID(0.00005, 0, 0.08, 'f', 80, 1);
+    //m2Speed += computePID(0.00008, 0, 0.08, 'f', 80, 2);
 
     if(m1Speed <= 0)
       m1Speed = 0;
@@ -101,7 +112,7 @@ void TestStraight()
     if(m2Speed <=0)
       m2Speed = 0;
       
-    md.setSpeeds(m1Speed, m2Speed);     
+    //md.setSpeeds(m1Speed, m2Speed);     
 }
 
 void Forward(double cm)
@@ -110,6 +121,21 @@ void Forward(double cm)
   m1TargetCounter = m2TargetCounter = 0;
 
   Serial.println("Start of forward");
+
+  //Open Loop
+
+  if(m1Speed == 0 || m2Speed == 0)
+  {
+    for(int i = 0; i < 4; ++ i)
+    {
+      md.setSpeeds(i*50, i*50);
+      delay(50);
+    }
+  }
+
+  m1Speed = 200;
+  m2Speed = 200;
+  
   while(m1TargetCounter < targetTick || m2TargetCounter < targetTick)
   {
     calcTicks (TICK_REFRESH_INTERVAL);
@@ -121,8 +147,8 @@ void Forward(double cm)
     Serial.print(m2RPM);
     Serial.println();
 
-    m1Speed += computePID(0.00025, 0, 0.0005, 'f', 88.0, 1);
-    m2Speed += computePID(0.00025, 0, 0.0005, 'f', 85.0, 2);
+    m1Speed += computePID(0.0025, 0, 0.0005, 'f', 40, 1);
+    m2Speed += computePID(0.008, 0, 0.0005, 'f', 40, 2);
     
     if(m1Speed <= 0)
       m1Speed = 0;
@@ -137,7 +163,71 @@ void Forward(double cm)
   Serial.print(", ");
   Serial.print(m2Speed);
   
-  md.setBrakes(380, 400);
+  md.setBrakes(390, 400);
+}
+
+void TurnRight()
+{
+   //Diameter of wheel to wheel is 17cm
+  //For a 90 deg turn, both wheels must turn the robot 45 degs in each direction
+  //By using the formula Pi * 17 * 45/360 
+  //We can find that the distance each wheel must travel is 6.67cm
+
+  //Turn 90 deg
+  //int targetTick = 6.67 * TICKS_PER_CM;
+  int targetTick = 13 * TICKS_PER_CM;
+  m1TargetCounter = m2TargetCounter = 0;
+
+ 
+  if(m1Speed == 0 || m2Speed == 0)
+  {
+    for(int i = 0; i < 3; ++ i)
+    {
+      md.setSpeeds(i*50, -i*50);
+      delay(50);
+    }
+    m1Speed = 200;
+    m2Speed = 200;
+  }
+  
+  while(m1TargetCounter < targetTick - 90 || m2TargetCounter < targetTick)
+  {
+    calcTicks (TICK_REFRESH_INTERVAL);
+  
+    Serial.print("Left RPM: ");
+    Serial.print(m1RPM);
+    
+    Serial.print(", Right RPM: ");
+    Serial.print(m2RPM);
+    Serial.println();
+
+    m1Speed += computePID(0.0020, 0, 0.0005, 'f', 40, 1);
+    m2Speed += computePID(0.0025, 0, 0.0005, 'f', 40, 2);
+    
+    if(m1Speed <= 0)
+      m1Speed = 0;
+
+    //Right turn the right motor must go backwards
+    if(m2Speed <= 0)
+      m2Speed = 0;
+ 
+    md.setSpeeds(m1Speed, -m2Speed);     
+  }
+
+  Serial.print(m1Speed);
+  Serial.print(", ");
+  Serial.print(m2Speed);
+  
+  md.setBrakes(400, 370);
+}
+
+void TurnLeft()
+{
+  
+}
+void Turn (int angle)
+{
+ TurnRight();
 }
 
 
@@ -154,24 +244,43 @@ void setup()
   md.init();
 }
 
+int i = 0;
+char input;
+char commands[7] = {'f', 'r', 'f', 'r', 'f', 'r', 'f'};
+
 void loop() 
 {  
   //To update RPM for use by PID
   calcTicks (TICK_REFRESH_INTERVAL);
 
-  
-  char input = 'f';
+  int commandSize = 7;
+  //TestStraight();
+  input = commands[i];
+
+  ++ i;
+
+
+
+
+  if(i > 7)
+  {
+    while(1);
+  }
+    
   switch(input)
   {
     case 'f':
     Forward(10);
-    delay(2000);
+    delay(1000);
+    ResetPID();
+    m1Speed = m2Speed = 0;    
     break;
 
     case 'r':
-       break;
+    Turn(45);
+    delay(1000);
+    ResetPID();
+    m1Speed = m2Speed = 0;
+    break;
   }
-  
-
-
 }
